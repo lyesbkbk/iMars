ae06a1b61436c9affe4cd3df8238f10926c6f747
Mariia Zhurina <mariia.zhurina@gmail.com>
2021-03-18 14:26:28 +0100

    Sidebar

--- iMarsUIKit/3DModels/ModelView.swift
+++ iMarsUIKit/3DModels/ModelView.swift
@@ -1,20 +1,20 @@
 //
-//  AllView.swift
-//  iMarsUIKit
+//  3dView.swift
+//  iMars
 //
 //  Created by Mariia Zhurina on 18/03/2021.
 //
 
 import SwiftUI
 
-struct AllView: View {
+struct ModelView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct AllView_Previews: PreviewProvider {
+struct ModelView_Previews: PreviewProvider {
     static var previews: some View {
-        AllView()
+        ModelView()
     }
 }
