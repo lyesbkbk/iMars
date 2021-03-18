ad1d04a246440872acd0a4a63b95542c708e60cc
Mariia Zhurina <mariia.zhurina@gmail.com>
2021-03-18 16:00:56 +0100

    comit

---
+++ iMarsUIKit/Libray/Photos/ApiPhotos.swift
@@ -0,0 +1,9 @@
+//
+//  ApiPhotos.swift
+//  iMarsUIKit
+//
+//  Created by Mariia Zhurina on 18/03/2021.
+//
+
+import Foundation
+
--- iMarsUIKit/Libray/Photos/PhotoView.swift
+++ iMarsUIKit/Libray/Photos/PhotoView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct PhotoView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        
+   }
 }
 
 struct PhotoView_Previews: PreviewProvider {
