39dc920051477d73d9f68380052703f663148fca
laura1708 <lowsco8@gmail.com>
2021-03-19 10:30:23 +0100

    add article model data + detailView of article with black mode + scene delegate L22 select view

--- iMarsUIKit/DetailViewArticle.swift
+++ iMarsUIKit/DetailViewArticle.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct DetailViewArticle: View {
+    let article: Article
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView   {
+            VStack {
+            Image(article.photo)
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth : 1150, maxHeight: 650)
+                .cornerRadius(5.0)
+
+                
+            
+            HStack {
+                Text(article.source)
+                    .foregroundColor(Color("rediMars"))
+                    .padding(/*@START_MENU_TOKEN@*/.horizontal/*@END_MENU_TOKEN@*/)
+                Spacer()
+                    
+                Text(article.date)
+                    .foregroundColor(Color("rediMars"))
+                    .padding(/*@START_MENU_TOKEN@*/.horizontal/*@END_MENU_TOKEN@*/)
+                    
+                
+            }
+            
+            
+            Text(article.titre)
+               
+      
+                .fontWeight(.medium)
+                .foregroundColor(Color("blueiMars"))
+                .multilineTextAlignment(.center)
+
+                .font(.custom("SF Compact", size: 30))
+
+                Text(article.contenu)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    .font(.custom("SF Compact", size: 17))
+
+            
+            }
+
+        }
+        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
     }
 }
 
@@ -17,7 +59,8 @@ struct DetailViewArticle_Previews: PreviewProvider {
     static var previews: some View {
         
         Landscape {
-            DetailViewArticle()
+            DetailViewArticle(article: Articles[1])
+                
         }
     }
 }
