f426ac63faf20ecd42d226fe8373f0ab81ea3c24
Mariia Zhurina <mariia.zhurina@gmail.com>
2021-03-19 09:06:56 +0100

    API done

--- iMarsUIKit/Libray/Photos/PhotoAPI.swift
+++ iMarsUIKit/Libray/Photos/PhotoAPI.swift
@@ -11,17 +11,15 @@ struct PhotoAPI {
     static func fetch(completionHandler: @escaping ((PhotoRoot) -> Void)) {
         let url = URL(string: "https://images-api.nasa.gov/search?q=mars")!
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let data = data, error == nil else {
-                print("Error calling the API")
-                return
-            }
-            let decoder = JSONDecoder()
-            do {
-                let photoCollection = try decoder.decode(PhotoRoot.self,
-                                                         from: data)
-                completionHandler(photoCollection)
-            } catch let error {
-                print("Error while decodiong json: \(error.localizedDescription)")
+            if let data = data, error == nil {
+                let decoder = JSONDecoder()
+                do {
+                    let photoCollection = try decoder.decode(PhotoRoot.self,
+                                                             from: data)
+                    completionHandler(photoCollection)
+                } catch let error {
+                    print("Error while decodiong json: \(error.localizedDescription)")
+                }
             }
         }
         task.resume()
